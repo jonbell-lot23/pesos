26011e68455d0fe95d435cf2d545b07504cb5a56
Jon Bell <github@lot23.com>
2025-02-02 18:33:35 +1300

    Fixing?

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
+// This code prevents multiple instances of PrismaClient in development
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
