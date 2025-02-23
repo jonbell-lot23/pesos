3a2c442ca53ceeac8232fcdad83d973f823de9e9
Jon Bell <github@lot23.com>
2025-02-24 10:22:33 +1300

    Trying to fix

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,27 +1,11 @@
 "use client";
-import React, { useEffect, useRef } from "react";
-import lottie from "lottie-web";
+
+import React from "react";
 
 export default function Spinner() {
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (containerRef.current) {
-      const anim = lottie.loadAnimation({
-        container: containerRef.current,
-        renderer: "svg",
-        loop: true,
-        autoplay: true,
-        path: "/fire_loader.json",
-      });
-
-      return () => anim.destroy();
-    }
-  }, []);
-
   return (
-    <div className="flex justify-center items-center py-4">
-      <div ref={containerRef} className="w-32 h-32"></div>
+    <div className="flex items-center justify-center">
+      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
     </div>
   );
 }
