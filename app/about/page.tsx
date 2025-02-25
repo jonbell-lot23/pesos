940b62ed278f3a4f718d03ad59b156b519516548
Jon Bell <github@lot23.com>
2025-02-25 20:13:17 +1300

    Better about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,14 +17,22 @@ export default function AboutPage() {
       <section className="space-y-4">
         <h2 className="text-2xl font-semibold">What's next</h2>
         <p className="text-gray-700 leading-relaxed">
-          Right now, PESOS.site is a simple backup service that runs once a day
-          to save your content. It's very much a prototype, and very few people
-          use it.
+          Right now, PESOS.site is a simple backup service. It's very much a
+          prototype, and very few people use it. If you see anything wrong,{" "}
+          <a href="mailto:jb@lot23.com">let me know</a>!
         </p>
         <p>
-          In time, it will evolve into something more - a platform where you can
-          host your own personalized page, like if LinkTree pulled all your
-          content together in one place.
+          In time, it may evolve into a platform where you can host your own
+          personalized page, like if LinkTree pulled all your content together
+          in one place with a searchable archive. But that may take some time.
+        </p>
+      </section>
+      <section className="space-y-4">
+        <h2 className="text-2xl font-semibold">Who are you?</h2>
+        <p className="text-gray-700 leading-relaxed">
+          Kia Ora! My name is Jon and I live in windy Wellington, New Zealand. I
+          love the web and I am trying to help make it a bit better. My website
+          is at <a href="https://lot23.com">lot23.com</a>.
         </p>
       </section>
     </div>
